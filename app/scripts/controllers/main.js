6424b954b5cf310f650ff37d4c2c5ab46a8cc713
Mmiller44 <Mmiller44@yahoo.com>
2014-04-03 13:35:29 -0400

    Installed Grunt, Node, Angular + Foundation

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('codequizApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
