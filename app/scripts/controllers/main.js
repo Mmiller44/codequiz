291d00a89aab36ac00942fbd826dff1478a3fe69
Mmiller44 <Mmiller44@yahoo.com>
2014-04-07 17:00:00 -0400

    Added a function that runs when a quiz subject gets clicked. The function makes a call to the database and is console logging all the quizzes for that specific title.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,17 +3,27 @@
 angular.module('codequizApp')
   .controller('MainCtrl', ['$scope','$resource', function($scope, $resource) {
 
-  	// This $resource is calling the API to fill the home page with data from the database.
-  	// The data is split into either front end or back end and looped through the view.
- 	var Quizzes = $resource('http://localhost:3000/get-specific-category/:category', {});
-  	$scope.front_quizzes = Quizzes.query({category: 'Front End'}, function() {
-  });
+	// This $resource is calling the API to fill the home page with data from the database.
+	// The data is split into either front end or back end and looped through the view.
+	var Quizzes = $resource('http://localhost:3000/get-specific-category/:category', {});
+	$scope.frontQuizzes = Quizzes.query({category: 'Front End'}, function() {
+	});
 
 
-  	// This $resource is calling the API to fill the home page with data from the database.
-  	// The data is split into either front end or back end and looped through the view.
-  	$scope.back_quizzes = Quizzes.query({category: 'Back End'}, function() {
-  });
+	// This $resource is calling the API to fill the home page with data from the database.
+	// The data is split into either front end or back end and looped through the view.
+	$scope.backQuizzes = Quizzes.query({category: 'Back End'}, function() {
+	});
 
+	// Declaring a function that gets called from the view.
+	// This function will handle loading the next page to display all the correct titles and quiz info.
+	$scope.loadTitles = function(title){
+		console.log(title);
+		var getTitles = $resource('http://localhost:3000/get-type-of/:category', {});  		
+		$scope.quiz_titles = getTitles.query({category: title}, function() {
+			console.log($scope.quiz_titles);
+	});
+
+	};
 
 }]);
