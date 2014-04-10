453f2a8b01e598736409c601dbf67b7bb1cf662d
Mmiller44 <Mmiller44@yahoo.com>
2014-04-09 23:45:50 -0400

    Set  variables for user data if the user exists in the database. The database is searched based on the provider_ID which is set by the 3rd party logins and is completely unique.

--- app/scripts/services/userServices.js
+++ app/scripts/services/userServices.js
@@ -15,15 +15,29 @@ angular.module('codequizApp')
 
 }])
 
-// Declaring a factory which will act as my API calls to the database.
+// Declaring a factory which will call the database to find a user based on their provider_ID
   .factory('findUser',['$resource','$rootScope',function($resource,$rootScope){
 
 	var Users = $resource('http://localhost:3000/find-specific-user/:provider_ID',{provider_ID: 'Github:34588'});
 
 	// This function calls the resource with the params Front End.
 	// Sets $rootScope variable for the template view to load.
-	var objectOne = Users.query({}, function() {
-			console.log(objectOne);
+	var userObject = Users.query({}, function() {
+
+			// if objectOne[0] is undefined, we know a user by that provider_ID does not exist.
+			if(userObject[0] === undefined)
+			{
+				// No user by that ID exists, and needs to be added to the database.
+				console.log('No user exists');
+
+			}else
+			{
+				// Setting the user $rootScope variables to match the returned data.
+				console.log(userObject[0].username);
+				$rootScope.firstName = userObject[0].first_name;
+				$rootScope.lastName = userObject[0].last_name;
+				$rootScope.providerID = userObject[0].provider_ID;
+			}
 		});
 
 }]);
