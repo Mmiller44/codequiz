db477529c1c449399f62470d3d3589949984e5b2
Mmiller44 <Mmiller44@yahoo.com>
2014-04-30 15:40:55 -0400

    Running more debugging on checking if my object it truly empty or not.

--- laravel/app/controllers/UserQuizController.php
+++ laravel/app/controllers/UserQuizController.php
@@ -17,10 +17,7 @@ class UserQuizController extends BaseController {
 
 		// If object is not empty, return it to front end.
 		// Else, add them to the database, and then return the newly created object.
-		if(!empty($object))
-		{
-			return $object;
-		}else
+		if(empty($object))
 		{
 			$addPosition = new UserQuiz;
 		 	$addPosition->user_ID = $userID;
@@ -28,6 +25,10 @@ class UserQuizController extends BaseController {
 		 	$addPosition->currentNumber = 0;
 		 	$addPosition->completed = 'no';
 			$addPosition->save();
+			
+		}else
+		{
+			return $object;
 		}
 
 	}
