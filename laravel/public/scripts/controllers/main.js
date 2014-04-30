83e9249776b0a2c6290e9c0dc19afaf5d2a6ddf8
Mmiller44 <Mmiller44@yahoo.com>
2014-04-30 01:30:57 -0400

    Testing twitter login v.0.6

--- laravel/public/scripts/controllers/main.js
+++ laravel/public/scripts/controllers/main.js
@@ -24,7 +24,7 @@ angular.module('codequizApp')
 
 	$scope.twitterLogin = function()
 	{
-		var twitter = $resource('http://codequiz.io/login-twitter');
+		var twitter = $resource('/login-twitter');
 		var returnedObject = twitter.get(function(){
 			console.log(returnedObject);
 		});
