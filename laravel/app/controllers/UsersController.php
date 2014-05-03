7d37a6627729bd47c3fb18bf3e71b2273e357aae
Mmiller44 <Mmiller44@yahoo.com>
2014-05-03 18:42:11 -0400

    Working on the login functionality again. Trying to get the callback data and url working.

--- laravel/app/controllers/UsersController.php
+++ laravel/app/controllers/UsersController.php
@@ -43,7 +43,7 @@ class UsersController extends BaseController {
 		$obj = Social::setDecoder(function($user) {
         	return json_decode($user, true);
     	});
-    	echo $user;
+    	echo $user + '';
 	}
 
 // USER
