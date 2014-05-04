bdb2de7d5c1a472d36703c1511f26caff9ef42e9
Mmiller44 <Mmiller44@yahoo.com>
2014-05-03 23:10:43 -0400

    fixing syntax error

--- laravel/public/scripts/controllers/main.js
+++ laravel/public/scripts/controllers/main.js
@@ -17,7 +17,7 @@ angular.module('codequizApp')
 			$cookieStore.put('name', returnedUserData.name);
 			$cookieStore.put('location',returnedUserData.location);
 			$cookieStore.put('profileImage', 'imageLink');
-			$cookieStore.put('website', returnedUserData.website);
+			$cookieStore.put('website', returnedUserData.url);
 			$cookieStore.put('providerID',returnedUserData.providerID);
 
   			$window.location.href = '#/home';
