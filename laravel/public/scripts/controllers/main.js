64f1e130bc74972db99595f878fe4a6a355d5784
Mmiller44 <Mmiller44@yahoo.com>
2014-05-03 22:52:16 -0400

    syntax error

--- laravel/public/scripts/controllers/main.js
+++ laravel/public/scripts/controllers/main.js
@@ -13,10 +13,10 @@ angular.module('codequizApp')
 		// If a user exists, push them to the home page and set rootScope variables.
 		if(returnedUserData.username)
 		{
-			$cookieStore.put('username',returnedUserData.username;
-			$cookieStore.put('name', = returnedUserData.name;
-			$cookieStore.put('location',returnedUserData.location;
-			$cookieStore.put('profileImage',returnedUserData.profileImage;
+			$cookieStore.put('username',returnedUserData.username);
+			$cookieStore.put('name', = returnedUserData.name);
+			$cookieStore.put('location',returnedUserData.location);
+			$cookieStore.put('profileImage',returnedUserData.profileImage);
 			$cookieStore.put('website', returnedUserData.website);
 
 			$cookieStore.put('providerID',returnedUserData.providerID);
