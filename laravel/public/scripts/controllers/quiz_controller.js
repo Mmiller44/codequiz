4bc526e1783c572694bffca61b6607146cc25167
Mmiller44 <Mmiller44@yahoo.com>
2014-05-05 19:06:12 -0400

    Testing

--- laravel/public/scripts/controllers/quiz_controller.js
+++ laravel/public/scripts/controllers/quiz_controller.js
@@ -24,8 +24,9 @@ angular.module('codequizApp')
 
     // Using promises and then to make sure my view only loads if the data is returned.
     getQuizPosition.getPosition().then(function(data) {
-        $scope.data = data[0];
-        console.log($scope.data);
+        $scope.data = data;
+        console.log($scope.data.resource);
+        console.log($scope.data.[0]);
         $scope.currentNumber = $scope.data.currentNumber;
         $scope.indicatorNumber = parseInt($scope.currentNumber + 1);
         console.log($scope.currentNumber);
