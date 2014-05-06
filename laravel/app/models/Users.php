f88a3c4fa7bf99397b52130411a82dc67789b786
Mmiller44 <Mmiller44@yahoo.com>
2014-05-06 01:28:09 -0400

    Testing

--- laravel/app/models/Users.php
+++ laravel/app/models/Users.php
@@ -4,5 +4,6 @@ class Users extends Eloquent {
 
 	protected $table = 'Users';
 	protected $fillable = array('user_ID','username','name','provider_ID','profileImage','location','website');
+	protected $primaryKey = 'user_ID';
 
 }
