d529ec3fee50ad9906fc24f0809ee821e6f453c8
Mmiller44 <Mmiller44@yahoo.com>
2014-05-07 19:07:30 -0400

    Percentage sign still doesn't show up. Will probably have to put that in manually and float it next to the numbers. Currently, just assigned a scope variable to represent the duration the numbers count for. If the user scores under a 50% then it takes 1 second, otherwise it takes 2 seconds.

--- laravel/public/scripts/controllers/score_controller.js
+++ laravel/public/scripts/controllers/score_controller.js
@@ -3,10 +3,18 @@
 angular.module('codequizApp')
   .controller('score_controller',['$scope','$rootScope','$window', function ($scope,$rootScope,$window) {
 
+  	$scope.duration = 2;
+
     if(!$rootScope.finalScore)
     {
     	$window.location.href = '#/home';
+
+    }else if($rootScope.finalScore < 50)
+    {
+    	$scope.duration = 1;
     }
+
+
 
 
 
