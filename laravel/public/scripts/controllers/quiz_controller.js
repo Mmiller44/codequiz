9b2309144b2e3ee03b392c99d69b7ce8d50cb5cd
Mmiller44 <Mmiller44@yahoo.com>
2014-05-07 16:55:19 -0400

    Testing console log for index of curly bracket.

--- laravel/public/scripts/controllers/quiz_controller.js
+++ laravel/public/scripts/controllers/quiz_controller.js
@@ -41,10 +41,11 @@ angular.module('codequizApp')
 
          $scope.indicatorNumber = $scope.currentNumber + 1;
 
-         $scope.testing = 'var counter = 0;
-                            for (var i = Things.length - 1; i >= 0; i--) {
+         $scope.testing = "var counter = 0;
+                            for (var i = Things.length - 1; i >= 0; i--)
+                            {
                                 Things[i]
-                            };';
+                            };";
 
     });
 
