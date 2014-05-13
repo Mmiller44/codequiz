897b51ea90fca0984f76d48954d561f0e3327679
Mmiller44 <Mmiller44@yahoo.com>
2014-05-13 01:16:46 -0400

    Needed to tell Eloquent that the primary key for the Quizzes table is quiz_ID not just id. Fixed and testing again.

--- laravel/app/models/Quizzes.php
+++ laravel/app/models/Quizzes.php
@@ -4,4 +4,5 @@ class Quizzes extends Eloquent {
 
 	protected $table = 'Quizzes';
 	protected $fillable = array('quiz_ID','main_category','sub_category','title','description','user_ID','quiz_ranking');
+	protected $primaryKey = 'quiz_ID';
 }
