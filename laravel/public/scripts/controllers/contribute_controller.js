db711a0a11b1a88d4a94320911b70be974b958c5
Mmiller44 <Mmiller44@yahoo.com>
2014-05-13 00:17:58 -0400

    Testing out the contribution page. I have added resources to call and update the quizzes table, the questions table and the newly created Created_quizzes table. This functionality should allow a user to add more questions to a quiz.

--- laravel/public/scripts/controllers/contribute_controller.js
+++ laravel/public/scripts/controllers/contribute_controller.js
@@ -112,12 +112,20 @@ angular.module('codequizApp')
 			var createdQuiz = $resource('http://codequiz.io/update-contribute-position/:quizID/:userID/:currentNumber/:completed');
 			var dataObject = createdQuiz.get({quizID: $scope.quizID, userID: $scope.userID, currentNumber: $scope.routeNumber, completed: 'No'}, function(){
 				console.log(dataObject);
+				if(dataObject.currentNumber)
+				{
+					$scope.currentNumber = dataObject.currentNumber;
+				}
 			});
 
 			// Now I need to store the actual question.
 			var addQuestion = $resource('http://codequiz.io/add-question/:question/:a/:b/:c/:d/:correctAnswer/:quizID/:quizCategoryID/:explanation');
 			var QuestionObject = createdQuiz.get({question: question.text, a: question.a, b: question.b, c: question.c, d: question.d, correctAnswer: question.correctAnswer, quizID: $scope.quizID, quizCategoryID: 1, explanation: question.explanation}, function(){
 				console.log(QuestionObject);
+				if(QuestionObject)
+				{
+					$window.location.href = '#/contribute/' + $scope.currentNumber;
+				}
 			});
 		}
 
