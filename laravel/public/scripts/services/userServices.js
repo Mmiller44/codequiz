f7d21545bcf431cbb7359bcf4776876b088f886e
Mmiller44 <Mmiller44@yahoo.com>
2014-05-15 19:15:32 -0400

    Added the new localStorage usage and the new way of checking if a user is logged in to all of the controllers. Much cleaner code and more efficient.

--- laravel/public/scripts/services/userServices.js
+++ laravel/public/scripts/services/userServices.js
@@ -34,12 +34,12 @@ angular.module('codequizApp')
 		var website = $cookieStore.get('website');
 		var profileImage = $cookieStore.get('profileImage');
 	}
-	
+
 	// Making an api call to add or update a user to my database. Data gets returned back.
 	var newUser = $resource('http://codequiz.io/add-new-user/:provider_ID/:username/:name/:location/:website/:profileImage');
 
 	// userObject holds all returned results
-	var userData = newUser.query({provider_ID: providerID, username: username, name: name, location: location, website: website, profileImage: profileImage}).$promise.then(function(userObject) {
+	var userData = newUser.get({provider_ID: providerID, username: username, name: name, location: location, website: website, profileImage: profileImage}).$promise.then(function(userObject) {
 		
 		console.log('Worafafdfasdfsdf');
 
