4fc6a173f568b6710dbf417b595db0c1c3c9df79
Mmiller44 <Mmiller44@yahoo.com>
2014-05-15 09:55:12 -0400

    Added a window width check on the landing page to show or hide the mobile view and desktop view. Testing it out now.

--- laravel/public/scripts/controllers/main.js
+++ laravel/public/scripts/controllers/main.js
@@ -3,6 +3,30 @@
 angular.module('codequizApp')
   .controller('MainCtrl', ['$scope','$resource','$rootScope','$window','getQuizCategory','$cookieStore','findTwitter','getFacebook', function($scope, $resource, $rootScope, $window, getQuizCategory,$cookieStore,findTwitter,getFacebook) {
 
+  	// If a user is logged in, push to home page.
+	var currentUser = $cookieStore.get('providerID');
+	console.log(currentUser);
+
+	if(currentUser)
+	{
+		$window.location.href = '#/home'
+	}
+
+	$scope.width = $window.innerWidth;
+
+	var checkWidth = function(width){
+		if(width > 800)
+		{
+			$scope.mobile = false;
+		}else
+		{
+			$scope.mobile = true;
+		}
+	};
+
+	// Calling my checkWidth function.
+	checkWidth($scope.width);
+
 	$scope.facebookLogin = function()
 	{
 		var facebook = $resource('/login-facebook');
