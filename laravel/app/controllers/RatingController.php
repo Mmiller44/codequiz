4b964cf62282c1cab7a47dc3f118cdff4dcec3f8
Mmiller44 <Mmiller44@yahoo.com>
2014-05-23 16:17:18 -0400

    The tables are being updated when a rating gets submitted, but unfortunately the Quizzes table is not getting the right value

--- laravel/app/controllers/RatingController.php
+++ laravel/app/controllers/RatingController.php
@@ -43,7 +43,7 @@ class RatingController extends BaseController {
 		$roundedAverage = round($averageRating);
 
 		$quizData = Quizzes::where('quiz_ID', '=', $quizID)->first();
-		$quizData->quiz_ranking = $roundedAverage;
+		$quizData->quiz_ranking = 3;
 		$quizData->save();
 	}
 
