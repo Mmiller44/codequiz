e524fe47d9b116412f311e43dad767f81d316531
Mmiller44 <Mmiller44@yahoo.com>
2014-05-26 20:13:06 -0400

    testing out new modal window

--- laravel/public/scripts/controllers/quiz_landing_controller.js
+++ laravel/public/scripts/controllers/quiz_landing_controller.js
@@ -84,4 +84,25 @@ angular.module('codequizApp')
 			$rootScope.quizID = ID;
 		}
 
+	// Authorization for a Facebook account. returns a URL, which I redirect to. User signs in and authorizes.
+	$scope.facebookLogin = function()
+	{
+		var facebook = $resource('/login-facebook');
+		var returnedObject = facebook.get(function(){
+
+			$window.location.href=returnedObject.login;
+			
+		});
+	};
+
+	// Authorization for a Twitter account. returns a URL, which I redirect to. User signs in and authorizes.
+	$scope.twitterLogin = function()
+	{
+		var twitter = $resource('/login-twitter');
+		var returnedObject = twitter.get(function(){
+
+			$window.location.href=returnedObject.login;
+			
+		});
+
 }]);
